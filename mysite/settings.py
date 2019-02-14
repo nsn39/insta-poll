0f1b5d2cb693ea577bbba1cc60b93c8bbbafc672
Nishan Poudel <34716481+nsn39@users.noreply.github.com>
2019-02-14 12:41:11 -0800

    Deleted something anonymous!!!

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dpr_)-*q)qv5hr-#s!ob*h&8)=dg#^ewlw5xn6-!!@bxa(@wyc'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
